df186ed0fe799449729feffada1e696e9581b6b1
Dany Gauthier <danygauthier57@yahoo.ca>
2022-03-24 11:25:17 -0400

    Created db schema, added sql dummy data file

---
+++ db/test.sql
@@ -0,0 +1,36 @@
+-- Insert dummy DATA
+INSERT INTO Gender (GenderName)
+VALUES ('Man'), ('Woman'), ('Other');
+
+INSERT INTO ProductImage (ImageName)
+VALUES ('Help.png');
+
+INSERT INTO Brand (BrandName)
+VALUES ('Adidas'), ('Reebok'), ('Vans');
+
+INSERT INTO ProductType (TypeName)
+VALUES ('Running'), ('Training'), ('Everyday');
+
+INSERT INTO Color (ColorName)
+VALUES ('Red'), ('Blue'), ('Green');
+
+INSERT INTO Product 
+(ProductName, ProductDescription, COLORID, BRANDID, IMAGEID, TYPEID)
+VALUES ('Superstar', 'Cool stuff', 1, 1, 1, 3);
+
+INSERT INTO User 
+(LastName, FirstName, Email, Password, BirthDate, GENDERID)
+VALUES 
+('Gauthier', 'Dany', 'danygauthier57@yahoo.ca', 'Abc123', '2002-06-08', 1),
+('Doe', 'John', 'john.doe@exemple.com', 'Abc123', '2002-06-08', 1),
+('Doe', 'Jane', 'jane.doe@exemple.com', 'Abc123', '2002-06-08', 1);
+
+INSERT INTO Wishlist (PRODUCTID, USERID)
+VALUES (1, 1);
+
+INSERT INTO Cart (PRODUCTID, USERID)
+VALUES (1, 2);
+
+INSERT INTO ImageProduct (IMAGEID, PRODUCTID)
+VALUES (1, 1);
+
