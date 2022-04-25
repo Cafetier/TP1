750969f7608dba6c5a1859afa6750c00d0e76fe1
Xavier Jean <jeax10713@gmail.com>
2022-04-25 18:21:03 -0400

    About tables with bootstrap

--- src/views/About.php
+++ src/views/About.php
@@ -7,47 +7,69 @@ include_once "../template/alert.php";
 ?>
 
 <section class="container">
-    <h1>This is an about page</h1>
+    <h1>Learn a little more about us</h1>
+
+
+    <table class="table table-hover">
+
+  <tbody>
+    <tr class="table-active">
+      <th scope="col"></th>
+      <td>-We are the #1 selling website in Canada for the shoe industry.</td>
+    </tr>      
+    
+    <tr class="table-active">
+      <th scope="col"></th>
+      <td>-We sold over 250 000 shoes last years wich makes us #1 in sales and reputation.</td>
+    </tr>   
+
+    <tr class="table-active">
+      <th scope="col"></th>
+      <td>-So if you're looking for a place to buy some nice kicks, search no further, we have everything.</td>
+    </tr>   
+
+  </tbody>
+</table>
+
+
+
+
+<table class="table table-hover">
+
+  <tbody>
+
+    <tr class="">
+      <th scope="row"></th>
+      <td>-We try to give 5% of our profits to organizations that help the people in need.</td>
+
+    </tr>
+    <tr class="">
+      <th scope="row"></th>
+      <td>-Shoes Pro Shop is not only a market place for nice shoes, it's also a way to give back to your community.</td>
+
+    </tr>
+   
+  </tbody>
+</table>
+
+
+
+
+
+
+
+
+    
+
+
+  
+
+
+
+
 </section>
 
 <?php require_once('../template/footer.php'); ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<table>
-        <tr>
-             
-             <td> -We are the #1 selling website in Canada for the shoe industry.</td> 
-        </tr>
-             
-        <tr>   
-             <td> -We sold over 250 000 shoes last years wich makes us #1 in sales and reputation.</td>
-             
-        </tr>
-
-        <tr>
-            <td>So if you're looking for a place to buy some nice kicks, search no further, we have everything.</td>
-
-      </table>
 
 
-    <trable>
-          <tr> 
-              <td> We try to give 5% of our profits to organizations that help the people in need.</td>
-         </tr>
-
-         <tr>
-              <td> Shoes Pro Shop is not only a market place for nice shoes, it's also a way to give back to your community.</td>
-          <tr>
-    </table>
-
-
-</body>
-</html>
