65265791ed8d2127832946413d6af72299056535
Xavier Jean <jeax10713@gmail.com>
2022-04-25 00:25:23 -0400

    About

--- src/views/About.php
+++ src/views/About.php
@@ -11,3 +11,43 @@ include_once "../template/alert.php";
 </section>
 
 <?php require_once('../template/footer.php'); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<table>
+        <tr>
+             
+             <td> -We are the #1 selling website in Canada for the shoe industry.</td> 
+        </tr>
+             
+        <tr>   
+             <td> -We sold over 250 000 shoes last years wich makes us #1 in sales and reputation.</td>
+             
+        </tr>
+
+        <tr>
+            <td>So if you're looking for a place to buy some nice kicks, search no further, we have everything.</td>
+
+      </table>
+
+
+    <trable>
+          <tr> 
+              <td> We try to give 5% of our profits to organizations that help the people in need.</td>
+         </tr>
+
+         <tr>
+              <td> Shoes Pro Shop is not only a market place for nice shoes, it's also a way to give back to your community.</td>
+          <tr>
+    </table>
+
+
+</body>
+</html>
